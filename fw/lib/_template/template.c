2a445714ce19b51c518671729ba8a58b215175db
Marian Šámal <margotka000@gmail.com>
2020-04-24 11:48:38 +0200

    create PlatformIO project

---
+++ fw/lib/_template/template.c
@@ -0,0 +1,3 @@
+// add your functions, classes and stuff here
+// I hope you know how C libraries work :)
+// have a look at `lib/README`, it's described over there
---
+++ fw/lib/_template/template.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// add your header definitions here, this file will be included using
+// `#include "template.h"` in `src/main.cpp`
